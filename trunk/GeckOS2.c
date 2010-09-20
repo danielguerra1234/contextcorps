936425644b1edd0860c29beb920001735d56ebf2
riveted321 <riveted321@67f52b86-7866-7047-cbb4-19b6a6cf348e>
2010-09-20 15:41:50 +0000

    fixed a lot of minor code errors, and added error handling Header and Source files

--- trunk/GeckOS2.c
+++ trunk/GeckOS2.c
@@ -14,6 +14,7 @@
 #include <sys/types.h>
 #include <dirent.h>
 
+#include "Errors.h"
 #include "MPX_SUPT.H"
 
 #define SIZE 10000
@@ -84,51 +85,50 @@ int parseCommand(char *commandString) {
 //	printf("command: %s\narg1: %s\narg2: %s\narg3: %s\narg4: %s\n", command,arg1,arg2,arg3,arg4); //XXX: DEBUG
 	if (command == NULL) return 0; //if no command given, simply return...since it's not really an issue
 	if (strcmp(command,"help") == 0) {
-		int returnCode = help(arg1);
-		if (returnCode < 0) printf("Could not find command '%s'\n",arg1);
+		help(arg1);
+		return 0;
 	}
-	else if (strcmp(command,"version") == 0) {
+	if (strcmp(command,"version") == 0) {
 		version();
+		return 0;
 	}
-	else if (strcmp(command,"clear") == 0) {
+	if (strcmp(command,"clear") == 0) {
 		clearScreen();
+		return 0;
 	}
-	else if (strcmp(command,"cd") == 0) {
+	if (strcmp(command,"cd") == 0) {
+		if (arg1 == NULL) printf("cd requires a path\n");
+		else changeDir(arg1);
+		return 0;
+	}
+	if (strcmp(command, "setprompt") == 0) {
+		if (arg1 == NULL) puts("Prompt argument cannot be blank");
+		else if (strlen(arg1 > 20)) puts("Prompt argument must be 20 characters or less");
+		else setPrompt(arg1);
+		return 0;
+	}
+	if (strcmp(command,"date") == 0) {
 		if (arg1 == NULL) {
-			printf("cd required a path\n");
+			displayDate(); //if the user wants to display the date
 			return 0;
 		}
-		else {
-			changeDir(arg1);
-			return 0;
-		}
-	}
-	else if (strcmp(command, "setprompt") == 0) {
-		if (arg1 != NULL) {
-			if (strlen(arg1) <= 20) {
-				setPrompt(arg1);
-			} else {
-				puts("Prompt argument must be 20 characters or less");
-			}
-		} else {
-			puts("You must enter a string to set the prompt to");
-		}
-	}
-	else if (strcmp(command,"date") == 0) {
-		if (arg1 == NULL) displayDate(); //if the user wants to display the date
-		else if (strcmp(arg1,"set") == 0) { //if the user wants to set the date
-			if (arg2 != NULL && arg3 != NULL && arg4 != NULL) {
-				changeDate(atoi(arg2),atoi(arg3),atoi(arg4));
-			}
+		if (strcmp(arg1,"set") == 0) { //if the user wants to set the date
+			if (arg2 == NULL && arg3 == NULL && arg4 == NULL) printf("There must be Year, Month, and Day arguments. Date not changed\n");
+			else changeDate(arg2,arg3,arg4);
 		} else { //invalid argument
 			printf("Argument '%s' is not defined for %s", arg1, command);
 		}
-	} else if (strcmp(command,"dir") == 0) {
+		return 0;
+	}
+	if (strcmp(command,"dir") == 0) {
 		listDir();
-	} else if (strcmp(command,"exit") == 0) {
+		return 0;
+	}
+	if (strcmp(command,"exit") == 0) {
 		return 1;
-	} else printf("%s is not a valid command. For a list of valid commands, type 'help'\n", command);
-	return 0; //by getting this far, a valid command was passed and run, so let's get another one
+	}
+	printf("%s is not a valid command. For a list of valid commands, type 'help'\n", command);
+	return 0;
 }
 
 void listDir() {
@@ -149,104 +149,6 @@ void setPrompt(char *s) {
 	prompt = s;
 }
 
-int errorCodeTranslator(int code) {
-
-	switch (code) {
-	case (-100):
-	printf("Invalid Device Code\n");
-	  return 0;
-
-	case(-101):
-	  printf("Invalid Op Code\n");
-	  return 0;
-
-	case(-102):
-	  printf("Invalid Character Position\n");
-	  return 0;
-
-	case (-103):
-	  printf("Read has failed\n");
-	  return 0;
-
-	case (-104):
-	  printf("Write has failed\n");
-	  return 0;
-
-	case (-105):
-	  printf("Invalid Memory Pointer\n");
-	  return 0;
-
-	case (-106):
-	  printf("Free Memory Failed\n");
-	  return 0;
-
-	case (-107):
-	  printf("Invalid Date\n");
-	  return 0;
-
-	case (-108):
-	  printf("Date Not Changed\n");
-	  return 0;
-
-	case (-109):
-	  printf("Invalid Directory\n");
-	  return 0;
-
-	case (-110):
-	  printf("Directoy Open\n");
-	  return 0;
-
-	case (-111):
-	  printf("No Directory is Open\n");
-	  return 0;
-
-	case (-112):
-	  printf("No More Directory Entries\n");
-	  return 0;
-
-	case (-113):
-	  printf("Name Buffer was Too Long\n");
-	  return 0;
-
-	case (-114):
-	  printf("The Directory is Closed\n");
-	  return 0;
-
-	case (-115):
-	  printf("Loading Failed\n");
-	  return 0;
-
-	case (-116):
-	  printf("File Not Found\n");
-	  return 0;
-
-	case (-117):
-	  printf("File Invalid\n");
-	  return 0;
-
-	case (-118):
-	  printf("Program Size Error\n");
-	  return 0;
-
-	case (-119):
-	  printf("Invaliid Load Address\n");
-	  return 0;
-
-	case (-120):
-	  printf("Memory allociation Error\n");
-	  return 0;
-
-	case (-121):
-	  printf("Free Memory Error\n");
-	  return 0;
-
-	case (-122):
-	  printf("Invalid Handler Address\n");
-	  return 0;
-	}
-	return -1; //failed to find error code
-}
-
 void displayDate() {
 	sys_get_date(date_p);
 	printf("%d-",date_p->year);
@@ -259,48 +161,55 @@ void clearScreen() {
  return;
 }
 
-void changeDate(int year, int month, int day) {
+void changeDate(char *year, char *month, char *day) {
 	int i;
 	date_rec reset;
 	date_rec *save_date;
 	printf("Original date:\n");
 	displayDate();
 
-	save_date->year = year;
-	save_date->month = month;
-	save_date->day = day;
+	save_date->year = atoi(year);
+	save_date->month = atoi(month);
+	save_date->day = atoi(day);
 
-	if (year > 1000 || year < 2013) {
-		if((month== 1 || month==3 || month==5 || month==7 || month== 8 || month==10 || month==12) && day>31 ) {
-			printf("Error Encountered: %d is greater than 31\n",save_date->day);
-			return;
-		}
-		if((month== 4 || month==6 || month==9 || month==11) || day>30)  {
-			printf("Error Encountered: %d is greater than 30\n",save_date->day);
-			return;
-		}
-		if (month == 2) {
-			if (year%4 == 0 && day > 30) {
-				printf("Error Encounterd: %d is an invalid day for month %d\n",save_date->day, save_date->month);
-				return;
-			}
-			else if(month== 2 && day>29)    {
-				printf("Error Encountered: %d is greater than 28 for month %d\n",save_date->day, save_date->month);
-				return;
-			}// End else if
-		} //End inside if
-	} //End Outer if
-	else {
-		printf("Invalid Year: %d",save_date->year);
+	if (save_date->year == NULL || save_date->month == NULL || save_date->day == NULL) {
+		errorCodeTranslator(ERR_SUP_INVDAT);
+		printf("Date arguments must all be Integers.\n");
+		return;
+	}
+
+	if (year < 1000 || year > 2011) {
+		errorCodeTranslator(ERR_INV_YEAR);
+		printf("Valid years are between 1000 and 2011");
+		return;
+	}
+	if (month < 1 || month > 12) {
+		errorCodeTranslator(ERR_INV_MONTH);
+		printf("Valid months are between 1 and 12");
+		return;
+	}
+	if (day < 1 || day > 31) {
+		errorCodeTranslator(ERR_INV_DAY);
+		printf("Valid days for this month are between 1 and 31");
+		return;
+	}
+	if((month== 4 || month==6 || month==9 || month==11) && day>30) {
+		errorCodeTranslator(ERR_SUP_INVDAT);
+		printf("Valid days for this month are between 1 and 30");
+		return;
+	}
+	//TODO: get leap years right
+	if (month == 2) {
+		if (year%4 == 0 && day > 30) errorCodeTranslator(ERR_SUP_INVDAT);
+		else if(month == 2 && day>29) errorCodeTranslator(ERR_SUP_INVDAT);
+		return;
+	}
+	if (save_date->year == date_p->year && save_date->month == date_p->month && save_date->day == date_p->day) {
+		errorCodeTranslator(ERR_SUP_DATNCH);
 	}
 	i = sys_set_date(save_date);
-	if (save_date->year == date_p->year && save_date->month == date_p->month && save_date->day == date_p->day)
-		 i = ERR_SUP_DATNCH;
-	if (i == ERR_SUP_INVDAT)
-		  printf("Error Code: %d\n Invalid Date\n",ERR_SUP_INVDAT);
 	if (i == OK) {
-		  printf("Date Successfully Set\n");
-		  printf("Date after setting:\n");
+		  printf("Date successfully set to:\n");
 		  displayDate();
 	}
 }
@@ -315,41 +224,41 @@ int help(char *command){
 				"cd : changes directory -- Does not require -arguement\n"
 				"exit : closes the system\n"
 				"setprompt : sets the prompt with the given argument");
-		return 0;
+		return;
 	}
 	if (strcmp(command,"help") == 0) {
 		printf("Congrats, you've figured out how this command works! Now use it on something useful.\n");
-		return 0;
+		return;
 	}
 	if (strcmp(command,"date") == 0) {
 		printf("This command displays the current date. Use with argument 'set year month day' to change the date instead, where year, month, and day are all integers.\n");
-		return 0;
+		return;
 	}
 	if (strcmp(command,"cd") == 0) {
 		printf("Changes the directory\n");
-		return 0;
+		return;
 	}
 	if (strcmp(command,"clear") == 0) {
 		printf("This command simply clears the screen\n");
-		return 0;
+		return;
 	}
 	if (strcmp(command,"version") == 0) {
 		printf("This command displays the current version of GeckOS.\n");
-		return 0;
+		return;
 	}
 	if (strcmp(command,"dir") == 0) {
 		printf("This command displays information about all the files: typically the name and size in bytes\n");
-		return 0;
+		return;
 	}
 	if (strcmp(command,"exit") == 0) {
 		printf("This command closes the program\n");
-		return 0;
+		return;
 	}
 	if (strcmp(command,"setprompt") == 0) {
-			printf("This command changes the prompt to the given argument. The argument must be 20 characters or less\n");
-			return 0;
-		}
-	return -1; //if it got this far, the command was not found
+		printf("This command changes the prompt to the provided argument. The argument must be 20 characters or less\n");
+		return;
+	}
+	printf("Could not find help for command: %s", command);
 }
 void version () {
 	printf("This is the version #1.0.23 of GeckOs\n");
